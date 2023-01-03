fb2ce68cb115e2207b67d9284c4dd8465c22a010
Martin Brantner <martinfbrantner@gmail.com>
2023-01-04 00:15:46 +0100

    allow summary generation to use CUDA for greater performance; add progress indicator to summarization process

--- implementation/summarize.py
+++ implementation/summarize.py
@@ -1,21 +1,24 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 from data.loader import Loader
+from tqdm import tqdm
 import pandas as pd
 
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
 tokenizer = T5Tokenizer.from_pretrained("t5-small",model_max_length=512)
 
-# TODO: IMPROVE PERFORMANCE
-device = torch.device('cpu')
+# check if we can use the GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
 
 print("Loading data...")
 loader = Loader()
 loader.loadTuples(100, 100) # TODO: FIT TO DATA WE REALLY USE OR WHOLE CORPUS IF RESOURCES!
 corpus = loader.getCorpus()
 
+progress_bar = tqdm(total=len(corpus), desc=f"Generating summaries using {device}", unit="docs")
 summarized_texts = []
-for index, text in enumerate(corpus['text']):
+for text in corpus['text']:
     preprocessed = text.strip().replace("\n","")
     text_to_summarize = "summarize: " + preprocessed
     text_as_tensors = tokenizer.encode(text_to_summarize,return_tensors='pt').to(device)
@@ -25,8 +28,9 @@ for index, text in enumerate(corpus['text']):
 
     summarized_texts.append(summary_text)
 
-    if(index%10 == 0):
-        print(index)
+    progress_bar.update(1)
+
+progress_bar.close()
 
 
 corpus['summarized'] = summarized_texts
