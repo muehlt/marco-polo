108b9854eb53d3a0a738bce550e811f15a515f2c
Martin Brantner <martinfbrantner@gmail.com>
2023-01-06 22:11:14 +0100

    adjust summarize.py to generate .jsonl file; generate summaries of reduced dataset (~9K entries)

--- implementation/summarize.py
+++ implementation/summarize.py
@@ -5,26 +5,26 @@ from tqdm import tqdm
 import pandas as pd
 
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
-tokenizer = T5Tokenizer.from_pretrained("t5-small",model_max_length=512)
+tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=512)
 
 # check if we can use the GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
 print("Loading data...")
-loader = Loader()
-loader.loadTuples(100, 100) # TODO: FIT TO DATA WE REALLY USE OR WHOLE CORPUS IF RESOURCES!
+loader = Loader(use_reduced=True)
+loader.loadTuples()
 corpus = loader.getCorpus()
 
 progress_bar = tqdm(total=len(corpus), desc=f"Generating summaries using {device}", unit="docs")
 summarized_texts = []
 for text in corpus['text']:
-    preprocessed = text.strip().replace("\n","")
+    preprocessed = text.strip().replace("\n", "")
     text_to_summarize = "summarize: " + preprocessed
     text_as_tensors = tokenizer.encode(text_to_summarize,return_tensors='pt').to(device)
 
     summary_tokens = model.generate(text_as_tensors,max_length=(len(preprocessed.split())-1))
-    summary_text = tokenizer.decode(summary_tokens[0],skip_special_tokens=True)
+    summary_text = tokenizer.decode(summary_tokens[0], skip_special_tokens=True)
 
     summarized_texts.append(summary_text)
 
@@ -33,7 +33,12 @@ for text in corpus['text']:
 progress_bar.close()
 
 
-corpus['summarized'] = summarized_texts
+corpus['summary'] = summarized_texts
 
-print(corpus[['text', 'summarized']])
+# write summarized docs to file
+with open("../data/msmarco/msmarco/summaries.jsonl", 'w') as file:
+    file.write(corpus.to_json(lines=True, orient='records'))
+
+
+print(corpus[['text', 'summary']])
 
