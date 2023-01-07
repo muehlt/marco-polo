856c79b7eefb1cd02a99a4372473982735c5ebbb
Matthias Paltauf <m.paltauf@student.tugraz.at>
2023-01-07 16:41:13 +0100

    Cosine similarity data transposed and recall, precision and f-score added

--- implementation/main.py
+++ implementation/main.py
@@ -2,6 +2,7 @@
 from data.fetcher import Fetcher
 from data.loader import Loader
 from data.processor import Processor
+from similarity import Similarity
 
 
 def main():
@@ -22,6 +23,12 @@ def main():
     q_doc_embedded    = query_processor.doPreprocessingStack()
 
     print(q_doc_embedded)
+
+    sim_object = Similarity(c_doc_embedded,q_doc_embedded)
+    sim_data = sim_object.calc_cosine_similarity_query_docs()
+    recall_dict = sim_object.recall(sim_data,0.9)
+    precision_dict = sim_object.precision(sim_data, 0.9)
+    f_score_dict = sim_object.f_score(recall_dict, precision_dict)
 
 if __name__ == '__main__':
     main()
--- implementation/similarity.py
+++ implementation/similarity.py
@@ -14,6 +14,37 @@ class Similarity:
         self.corpus = corpus
         self.queries = queries
 
+    def recall(self, data, threshold):
+        result_dict = {}
+        for i,col in enumerate(data.columns):
+            retrieved_list = data[i].loc[data[i] >= threshold].index.tolist()
+            query_id = self.queries['_id'][i]
+            test = pd.DataFrame(pd.read_csv("../data/msmarco/msmarco/qrels/test.tsv", sep="\t"))
+            base_relevant = test['corpus-id'].loc[test['query-id'] == query_id].tolist()
+            intersection_relevant_retrieved = set(retrieved_list) & set(base_relevant)
+            recall = len(intersection_relevant_retrieved) / len(base_relevant)
+            result_dict[query_id] = recall
+        return result_dict
+
+    def precision(self, data, threshold):
+        result_dict = {}
+        for i,col in enumerate(data.columns):
+            retrieved_list = data[i].loc[data[i] >= threshold].index.tolist()
+            query_id = self.queries['_id'][i]
+            test = pd.DataFrame(pd.read_csv("../data/msmarco/msmarco/qrels/test.tsv", sep="\t"))
+            base_relevant = test['corpus-id'].loc[test['query-id'] == query_id].tolist()
+            intersection_relevant_retrieved = set(retrieved_list) & set(base_relevant)
+            recall = len(intersection_relevant_retrieved) / len(retrieved_list)
+            result_dict[query_id] = recall
+        return result_dict
+
+    def f_score(self,recall_dict,precision_dict):
+        f_score_dict = {}
+        for query in enumerate(recall_dict.keys()):
+            f_score = 2*precision_dict[query]*recall_dict[query] / (precision_dict[query]+recall_dict[query])
+            f_score_dict[query] = f_score
+        return f_score_dict
+
     def calc_cosine_similarity_query_docs(self):
 
         similarity_data = pd.DataFrame()
@@ -27,5 +58,6 @@ class Similarity:
 
             df_query_results = pd.DataFrame([query_results])
             similarity_data = pd.concat([similarity_data, df_query_results], ignore_index=True)
+        similarity_data_transpose = similarity_data.transpose()
 
-        return similarity_data
+        return similarity_data_transpose
