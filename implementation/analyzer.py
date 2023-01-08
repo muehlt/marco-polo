39b20b44f85863867a489ed029f6c750bb0df1e4
Fabian Staber <fabian.staber@student.tugraz.at>
2023-01-08 15:45:26 +0100

    refactor data analysis

---
+++ implementation/analyzer.py
@@ -0,0 +1,47 @@
+from matplotlib import pyplot as plt
+
+
+class Analyzer:
+
+    def __init__(self, c_recall, c_precision, c_fscore,
+                 s_recall, s_precision, s_fscore):
+        self.c_recall = c_recall
+        self.c_precision = c_precision
+        self.c_fscore = c_fscore
+        self.s_recall = s_recall
+        self.s_precision = s_precision
+        self.s_fscore = s_fscore
+        self.keys = list(range(0, 43))
+
+    def scatter_plot_recall(self, threshold):
+        plt.figure(1)
+        ax = plt.axes()
+        ax.scatter(self.keys, self.c_recall.values(), label="Corpus")
+        ax.scatter(self.keys, self.s_recall.values(), label="Summaries")
+        ax.set_title(f"Recall \n t = {threshold}")
+        ax.set_ylabel('Recall score')
+        ax.set_xlabel('Query IDs')
+        ax.legend()
+        plt.show()
+
+    def scatter_plot_precision(self, threshold):
+        plt.figure(2)
+        ax = plt.axes()
+        ax.scatter(self.keys, self.c_recall.values(), label="Corpus")
+        ax.scatter(self.keys, self.s_recall.values(), label="Summaries")
+        ax.set_title(f"Precision \n t = {threshold}")
+        ax.set_ylabel('Precision score')
+        ax.set_xlabel('Query IDs')
+        ax.legend()
+        plt.show()
+
+    def scatter_plot_fscore(self, threshold):
+        plt.figure(3)
+        ax = plt.axes()
+        ax.scatter(self.keys, self.c_fscore.values(), label="Corpus")
+        ax.scatter(self.keys, self.s_fscore.values(), label="Summaries")
+        ax.set_title(f"F-Score \n t = {threshold}")
+        ax.set_ylabel('F-Score')
+        ax.set_xlabel('Query IDs')
+        ax.legend()
+        plt.show()
--- implementation/main.py
+++ implementation/main.py
@@ -2,8 +2,8 @@
 from data.fetcher import Fetcher
 from data.loader import Loader
 from data.processor import Processor
+from analyzer import Analyzer
 from similarity import Similarity
-import matplotlib.pyplot as plt
 
 def main():
     fetcher = Fetcher()
@@ -52,40 +52,12 @@ def main():
     print("Corpus F-Scores: ", c_fscore_dict)
     print("Summaries F-Scores: ", s_fscore_dict)
 
-
-    keys = list(range(0, 43))
-    # scatter plot recall per query for corpus & summarized
-    plt.figure(1)
-    recall = plt.axes()
-    recall.scatter(keys, c_recall_dict.values(), label="Corpus")
-    recall.scatter(keys, s_recall_dict.values(), label="Summaries")
-    recall.set_title(f"Recall \n t = {threshold}")
-    recall.set_ylabel('Recall score')
-    recall.set_xlabel('Query IDs')
-    recall.legend()
-    plt.show()
-
-    # scatter plot precision per query for corpus & summarized
-    plt.figure(2)
-    prec = plt.axes()
-    prec.scatter(keys, c_precision_dict.values(), label="Corpus")
-    prec.scatter(keys, s_precision_dict.values(), label="Summaries")
-    prec.set_title(f"Precision \n t = {threshold}")
-    prec.set_ylabel('Precision score')
-    prec.set_xlabel('Query IDs')
-    prec.legend()
-    plt.show()
-
-    # scatter plot fscore per query for corpus & summarized
-    plt.figure(3)
-    fscore = plt.axes()
-    fscore.scatter(keys, c_fscore_dict.values(), label="Corpus")
-    fscore.scatter(keys, s_fscore_dict.values(), label="Summaries")
-    fscore.set_title(f"F-Score \n t = {threshold}")
-    fscore.set_ylabel('F-Score')
-    fscore.set_xlabel('Query IDs')
-    fscore.legend()
-    plt.show()
+    # plotting
+    data_analyzer = Analyzer(c_recall_dict, c_precision_dict, c_fscore_dict,
+                             s_recall_dict, s_precision_dict, s_fscore_dict)
+    data_analyzer.scatter_plot_recall(threshold)
+    data_analyzer.scatter_plot_precision(threshold)
+    data_analyzer.scatter_plot_fscore(threshold)
 
 
 if __name__ == '__main__':
