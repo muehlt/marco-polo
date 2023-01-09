6f934144a53c863be177edcdeb682135de0792c0
Fabian Staber <fabian.staber@student.tugraz.at>
2023-01-09 20:51:34 +0100

    remove unneeded comments

--- implementation/similarity.py
+++ implementation/similarity.py
@@ -25,7 +25,6 @@ class Similarity:
             intersection_relevant_retrieved = set(retrieved_list) & set(base_relevant)
             recall = len(intersection_relevant_retrieved) / len(base_relevant)
             result_dict[query_id] = recall
-        #print("Recall mean "+ str(np.mean(list(result_dict.values()))))
         return result_dict
 
     def precision(self, data, threshold):
@@ -37,7 +36,6 @@ class Similarity:
             intersection_relevant_retrieved = set(retrieved_list) & set(base_relevant)
             precision = len(intersection_relevant_retrieved) / len(retrieved_list)
             result_dict[query_id] = precision
-        #print("Precision mean " + str(np.mean(list(result_dict.values()))))
         return result_dict
 
     def f_score(self, recall_dict, precision_dict):
@@ -45,12 +43,10 @@ class Similarity:
         for query in recall_dict.keys():
             f_score = 0 if (precision_dict[query]+recall_dict[query]) == 0 else 2*precision_dict[query]*recall_dict[query] / (precision_dict[query]+recall_dict[query])
             f_score_dict[query] = f_score
-        #print(np.mean(list(f_score_dict.values())))
         return f_score_dict
 
     def calc_cosine_similarity_query_docs(self, dataset_name):
 
-        # TODO: remove if calculation should be done everytime, comment out if data changes
         path = f"../data/msmarco/msmarco/cosine_similarity_{dataset_name}.tsv"
         if os.path.isfile(path):
             df = pd.read_csv(path, sep="\t", index_col=0)
@@ -69,7 +65,6 @@ class Similarity:
             similarity_data = pd.concat([similarity_data, df_query_results], ignore_index=True)
         similarity_data_transpose = similarity_data.transpose()
 
-        # TODO: remove if calculation should be done everytime
         similarity_data_transpose.to_csv(path, sep="\t")
 
         return similarity_data_transpose
