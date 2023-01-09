37d401c6ef02a36d38c8c9c107abfcda061730b6
Martin Brantner <martinfbrantner@gmail.com>
2023-01-09 19:52:48 +0100

    change debug output to only display meaningful data

--- implementation/main.py
+++ implementation/main.py
@@ -6,6 +6,9 @@ from data.processor import Processor
 from analyzer import Analyzer
 from similarity import Similarity
 from data.training import Training
+import numpy as np
+def print_results():
+    pass
 
 def main():
 
@@ -38,25 +41,35 @@ def main():
 
     #print(q_doc_embedded)
 
-    print(c_doc_embedded, s_doc_embedded)
+    #print(c_doc_embedded, s_doc_embedded)
 
     threshold = 0.615
+
+    # CORPUS
     c_sim_object = Similarity(c_doc_embedded,q_doc_embedded,test)
     c_sim_data = c_sim_object.calc_cosine_similarity_query_docs("corpus")
     c_recall_dict = c_sim_object.recall(c_sim_data,threshold)
     c_precision_dict = c_sim_object.precision(c_sim_data,threshold)
-    print(c_precision_dict, c_recall_dict)
+    #print(c_precision_dict, c_recall_dict)
     c_fscore_dict = c_sim_object.f_score(c_recall_dict, c_precision_dict)
+    print(f"Corpus Recall mean {np.mean(list(c_recall_dict.values()))}")
+    print(f"Corpus Precision mean: {np.mean(list(c_precision_dict.values()))}")
+    print(f"Corpus F-Score mean: {np.mean(list(c_fscore_dict.values()))}")
 
+    print("====================================")
 
+    # SUMMARY
     s_sim_object = Similarity(s_doc_embedded,q_doc_embedded,test)
     s_sim_data = s_sim_object.calc_cosine_similarity_query_docs("summarized")
     s_recall_dict = s_sim_object.recall(s_sim_data,threshold)
     s_precision_dict = s_sim_object.precision(s_sim_data,threshold)
     s_fscore_dict = s_sim_object.f_score(s_recall_dict, s_precision_dict)
+    print(f"Corpus Recall mean {np.mean(list(s_recall_dict.values()))}")
+    print(f"Corpus Precision mean: {np.mean(list(s_precision_dict.values()))}")
+    print(f"Corpus F-Score mean: {np.mean(list(s_fscore_dict.values()))}")
 
-    print("Corpus F-Scores: ", c_fscore_dict)
-    print("Summaries F-Scores: ", s_fscore_dict)
+    #print("Corpus F-Scores: ", c_fscore_dict)
+    #print("Summaries F-Scores: ", s_fscore_dict)
 
     # plotting
     data_analyzer = Analyzer(c_recall_dict, c_precision_dict, c_fscore_dict,
@@ -67,6 +80,7 @@ def main():
     data_analyzer.boxplot_recall_differences(threshold)
     data_analyzer.boxplot_precision_differences(threshold)
     data_analyzer.boxplot_fscore_differences(threshold)
+
 
 
 if __name__ == '__main__':
